b5b0ec488a0c592592c34ceead4db055182e550f
Deepak Dargade <deepakdargade@gmail.com>
2022-01-30 23:56:03 +0530

    chore: stripe billing portal added

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   resources :posts
   get 'pricing', to: 'static_pages#pricing'
   post 'checkout/create', to: 'checkout#create', as: 'checkout_create'
+  post 'billing_portal/create', to: 'billing_portal#create', as: 'billing_portal_create'
 end
