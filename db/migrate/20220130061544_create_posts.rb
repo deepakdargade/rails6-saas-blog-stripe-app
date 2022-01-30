0bf56d49886c7b77ad5802e9fd9311539f37208d
Deepak Dargade <deepakdargade@gmail.com>
2022-01-30 12:06:45 +0530

    chore: devise gem added

---
+++ db/migrate/20220130061544_create_posts.rb
@@ -0,0 +1,11 @@
+class CreatePosts < ActiveRecord::Migration[6.0]
+  def change
+    create_table :posts do |t|
+      t.string :title, null: false
+      t.text :content, null: false
+      t.boolean :is_premium, default: false
+
+      t.timestamps
+    end
+  end
+end
