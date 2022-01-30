5e131d0c168f918c2869d501e54c5b4c7613e988
Deepak Dargade <deepakdargade@gmail.com>
2022-01-30 12:34:56 +0530

    chore: stripe customer id added to user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_30_063845) do
+ActiveRecord::Schema.define(version: 2022_01_30_070005) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "title", null: false
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2022_01_30_063845) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "plan"
     t.string "subscription_status", default: "incomplete"
+    t.string "stripe_customer_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
