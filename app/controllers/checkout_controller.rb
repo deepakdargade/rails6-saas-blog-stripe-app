d26b6b40c06cb09ed1cf7216abc40b213f420363
Deepak Dargade <deepakdargade@gmail.com>
2022-01-30 23:48:19 +0530

    chore: stripe checkout added for basic free and paid plan

--- app/controllers/checkout_controller.rb
+++ app/controllers/checkout_controller.rb
@@ -2,11 +2,12 @@ class CheckoutController < ApplicationController
   
     def create
         @session = Stripe::Checkout::Session.create({
+            customer: current_user.stripe_customer_id,
             success_url: posts_url,
             cancel_url: pricing_url,
             payment_method_types: ['card'],
             line_items: [{
-                price: 'price_1KNXcSLQynuac4il18YKmx0F',
+                price: params[:price],
                 quantity: 1
             }],
             mode: 'subscription'
